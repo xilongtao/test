4bd821ad9576caf91c17b9e197a32438dcbb75f6
xilongtaoPC <xilongtao@sporte.cn>
2017-07-25 14:26:18 +0800

    update

---
+++ Hello.php
@@ -0,0 +1,8 @@
+<?php
+class Hello
+{
+    public static function sayHello()
+    {
+        echo 'hello world';
+    }
+}
--- hello.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Hello {
-  public static function sayHello()
-  {
-    echo 'hello world';
-  }
-}
